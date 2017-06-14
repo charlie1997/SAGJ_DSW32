bb8d12f9853fa768953ea7240dfb1476178d43bc
charlie1997 <juancarlos_sg@live.com>
2017-06-14 11:35:07 -0500

    Trabajos - Juan Carlos Sanhez Gonzalez
    Grado & Especialidad: DSW - TI32

---
+++ operacion.php
@@ -0,0 +1,6 @@
+<?php
+$n1=(isset($_REQUEST["n1"])?($_REQUEST["n1"]:0);
+$n2=(isset($_REQUEST["n2"])?($_REQUEST["n2"]:0);
+(operacion) ?si:no;
+echo $n1+$n2;
+?>
---
+++ peticion.php
@@ -0,0 +1,13 @@
+<?php
+<<<<<<< HEAD
+	echo '<p id="nombre">'.$_GET['nombre']."</p>";
+	echo '<p id="edad">'.$_GET['edad']."</p>";
+	?>
+    
+	
+=======
+echo '<p id="nombre">'.$_GET['nombre']."</p>";
+echo '<p id="edad">'.$_GET['edad']."</p>";
+
+?>
+>>>>>>> fd5ac508189223a06fe657f328139e39cfaffde3
